6a9a3d0c80141d8f2203c4e25d6f1da9a6c85544
Trevon Wiggs <tre@fb.com>
2018-06-27 13:02:36 -0700

    first commit

--- flix/View Controllers/MoviesViewController.h
+++ flix/View Controllers/MoviesViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MoviesViewController.h
 //  flix
 //
 //  Created by Trevon Wiggs on 6/27/18.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MoviesViewController : UIViewController
 
 @end
-
